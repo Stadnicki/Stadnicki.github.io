0f01b0bc52ef32dc49c66854509fa656f628d268
Stadnicki <adrianpatrykstadnicki@gmail.com>
2017-10-14 18:59:47 +0200

    First commit

---
+++ dev_script.js
@@ -0,0 +1,5 @@
+function SignUp(my_form)
+  {
+    my_form.user_email.value;
+    my_form.user_pass.value;
+  }
---
+++ fire_scripts.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA8HsEo7SFBogFORGbqsZHzwJB5amZBX8M",
+    authDomain: "cards-9c053.firebaseapp.com",
+    databaseURL: "https://cards-9c053.firebaseio.com",
+    projectId: "cards-9c053",
+    storageBucket: "cards-9c053.appspot.com",
+    messagingSenderId: "669865222479"
+  };
+  firebase.initializeApp(config);
