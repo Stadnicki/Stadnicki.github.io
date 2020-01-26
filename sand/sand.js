69e3d1518a2d9417a7c32514eb2f6916f973ff05
Adrian Stadnicki <adrianpatrykstadnicki@gmail.com>
2020-01-26 16:26:56 +0100

    update sand

--- sand/sand.js
+++ sand/sand.js
@@ -9,7 +9,10 @@ const sandColorInput = document.getElementById('sandColorInput');
 const shapeColorInput = document.getElementById('shapeColorInput');
 const backgroundColorInput = document.getElementById('backgroundColorInput');
 const shapeOptionsSelect = document.getElementById('shapeOptionsSelect');
+const algorithmOptionsSelect = document.getElementById('algorithmOptionsSelect');
+let boundaryAbsorbing = false;
 
+let nextStepFunction;
 let sandDensity = 30;
 let simulationSpeed = 30;
 let sandColor = "#c2b280";
@@ -21,6 +24,29 @@ let shapeOptions = Object.keys(sandContainers);
 
 window.onload = () => {
     init();
+}
+
+const margolusTransformation = {
+    '0020': '2000',
+    '0002': '0200',
+    '2020': '0220',
+    '0220': '2200',
+    '2002': '2200',
+    '0202': '2200',
+    '2022': '2220',
+    '0222': '2202',
+    '0022': '2200'
+}
+
+const margolusTransformationForBoundaryAbsorbing = {
+    ...margolusTransformation,
+    '1122': '1100',
+    '1020': '1000',
+    '0102': '0100',
+    '1121': '1101',
+    '1112': '1110',
+    '1102': '1100',
+    '1120': '1100'
 }
 
 const init = () => {
@@ -41,6 +67,20 @@ const copyTwoDimArray = (array) => {
 
 const prepareSimulation = () => {
     clearInterval(simulationInterval);
+    switch(algorithmOptionsSelect.value) {
+        case 'margolus':
+            boundaryAbsorbing = false;
+            nextStepFunction = margolusNeighborhoodNextStep;
+            break;
+        case 'stadnicki':
+            boundaryAbsorbing = false;
+            nextStepFunction = nextStep;
+            break;
+        case 'boundary':
+            boundaryAbsorbing = true;
+            nextStepFunction = margolusNeighborhoodNextStep;
+            break;
+    }
     sandDensity = sandDensitySlider.value;
     simulationSpeed = simulationSpeedSlider.value;
     sandColor = sandColorInput.value;
@@ -70,26 +110,30 @@ const showSettings = () => {
     canvasDiv.style.display = "none";
 }
 
+const printCell = (array, x, y) => {
+    switch(array[y][x]) {
+        case 1:
+            ctx.fillStyle = shapeColor;
+            ctx.fillRect(x*10, y*10, 10, 10);
+            break;
+        case 2:
+            ctx.fillStyle = sandColor;
+            ctx.fillRect(x*10, y*10, 10, 10);
+            break;
+        case 0:
+            ctx.fillStyle = backgroundColor;
+            ctx.fillRect(x*10, y*10, 10, 10);
+            break;
+    }
+}
+
 const drawFromArray = (array) => {
     const xArraySize = array.length;
     const yArraySize = array[0].length;
 
     for(let x = 0; x < xArraySize; x++) {
         for(let y = 0; y < yArraySize; y++) {
-            switch(array[y][x]) {
-                case 1:
-                    ctx.fillStyle = shapeColor;
-                    ctx.fillRect(x*10, y*10, 10, 10);
-                    break;
-                case 2:
-                    ctx.fillStyle = sandColor;
-                    ctx.fillRect(x*10, y*10, 10, 10);
-                    break;
-                case 0:
-                    ctx.fillStyle = backgroundColor;
-                    ctx.fillRect(x*10, y*10, 10, 10);
-                    break;
-            }
+            printCell(array, x, y);
         }
     }
 }
@@ -123,6 +167,31 @@ const shuffleArray = (array) => {
         const temp = array[i]
         array[i] = array[j]
         array[j] = temp
+    }
+}
+
+const updateMargolusCell = (sourceArray, cellArray) => {
+    cellArray.forEach(coords => {
+        printCell(sourceArray, coords[0], coords[1])
+    });
+} 
+
+const margolusNeighborhoodNextStep = (array) => {
+    const xArraySize = array.length;
+    const yArraySize = array[0].length;
+    let margolusMap;
+    margolusMap = boundaryAbsorbing ? margolusTransformationForBoundaryAbsorbing : margolusTransformation;
+    for(let y = yArraySize - 1; y > 0; y --) {
+        for(let x = 0; x < xArraySize - 1; x++) {
+            const currentCell = '' + array[y][x] + array[y][x+1] + array[y-1][x] + array[y-1][x+1];    
+            if(currentCell in margolusMap) {
+                array[y][x] = parseInt(margolusMap[currentCell][0]);
+                array[y][x+1] = parseInt(margolusMap[currentCell][1]);
+                array[y-1][x] = parseInt(margolusMap[currentCell][2]);
+                array[y-1][x+1] = parseInt(margolusMap[currentCell][3]);
+                updateMargolusCell(array, [[x, y], [x+1, y], [x, y-1], [x+1, y-1]]);
+            }
+        }
     }
 }
 
@@ -176,5 +245,5 @@ const drawRectangle = (x, y, color) => {
 }
 
 const simulation = () => {
-    nextStep(arrayToWorkOn);
+    nextStepFunction(arrayToWorkOn);
 }
